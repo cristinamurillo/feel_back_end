87610f1e720a506e937bddb68328da6dd5b1067d
Cristina Murillo <murillocam96@gmail.com>
2018-12-20 16:52:32 -0500

    log in and log out, auth set up

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,11 +3,14 @@ class Api::V1::UsersController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
     def index
-    
         render json: User.all 
     end
+
+    def profile 
+        render json: {user: UserSerializer.new(current_user)}, status: :accepted 
+    end
+    
     def create 
-        
         @user = User.create(user_params)
      
         if @user.valid?
