44789b8591db3b04fbcad7018dec20b30a25d362
Cristina Murillo <murillocam96@gmail.com>
2019-01-02 17:33:08 -0500

    email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
     has_many :user_paintings
     has_many :paintings, through: :user_paintings
 
-    validates :email, presence: true, uniqueness: true 
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP,
+    message: "is invalid" }  
     validates :password, presence: true, length: {in: 6..30} 
     validates :first_name, length:{maximum: 20}, format: { with: /\A[a-zA-Z]+\z/,
     message: "only allows letters" }
